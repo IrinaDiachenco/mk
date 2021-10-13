69b2d61cefe500e534904e799abf4011b919d98d
IrinaDiachenco <bilairina@ukr.net>
2021-10-13 18:35:20 +0300

    Task #0

---
+++ main.js
@@ -0,0 +1,20 @@
+const player1 = {
+    name: 'Scorpion',
+    hp: 100,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+    weapon: ['Saw hat', 'Naginata', 'Steel fans', 'Hammer of Wrath'],
+    attack: function() {
+        console.log('Scorpion ' + 'Fight...')
+    }
+}
+
+const player2 = {
+    name: 'Kitana',
+    hp: 100,
+    img: '- http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
+    weapon: ['Saw hat', 'Naginata', 'Steel fans', 'Hammer of Wrath'],
+    attack: function() {
+        console.log('Kitana ' + 'Fight...')
+    }
+}
+
