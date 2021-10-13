d3426179b543e3091f57e30c0701cbb971120ac6
IrinaDiachenco <bilairina@ukr.net>
2021-10-13 19:47:01 +0300

    Task #1

--- main.js
+++ main.js
@@ -18,3 +18,32 @@ const player2 = {
     }
 }
 
+// function createPlayer() {
+    const $player1 = document.createElement('div');
+    // $player1.innerText = 'Scorpion';
+    $player1.classList.add('player1');
+    const $root = document.querySelector('.root');
+    $root.appendChild($player1);
+    const $progressbar = document.createElement('div');
+    $player1.appendChild($progressbar);
+    $progressbar.classList.add('progressbar');
+    const $life = document.createElement('div');
+    $progressbar.appendChild($life);
+    $life.classList.add('life');
+    $life.style.width = '100%'
+    const $name = document.createElement('div');
+    $progressbar.appendChild($name);
+    $name.classList.add('name');
+    $name.innerText = 'SCORPION';
+
+    const $character = document.createElement('div');
+    $player1.appendChild($character);
+    $character.classList.add('character');
+    const $img = document.createElement('img');
+    $img.src = 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif'
+    $character.appendChild($img);
+    
+
+// }
+
+// console.log(createPlayer())
